d9138b9a1226010f443573877cd80b9dbff691af
Abdul Osman <39681296+osmaniacattack@users.noreply.github.com>
2024-03-03 20:13:23 -0800

    Add console statements to link copier

--- contentScript.js
+++ contentScript.js
@@ -1,27 +1,26 @@
 // contentScript.js
 
 function handleImageClick(event) {
-    var imgSrc = event.target.src;
-    navigator.clipboard.writeText(imgSrc)
-      .then(() => {
-        console.log('Image source copied to clipboard:', imgSrc);
-      })
-      .catch(err => {
-        console.error('Failed to copy image source:', err);
-      });
-  }
-  
-  function startImageListener() {
-    document.addEventListener('click', function(event) {
-      if (event.target.tagName === 'IMG') {
-        handleImageClick(event);
-      }
+  var imgSrc = event.target.src;
+  console.log(imgSrc);
+  alert('Image source copied to clipboard:\n\n' + imgSrc);
+  navigator.clipboard.writeText(imgSrc)
+    .catch(err => {
+      console.error('Failed to copy image source:', err);
     });
-  }
-  
-  chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-    if (message.action === 'startImageListener') {
-      startImageListener();
+}
+
+function startImageListener() {
+  document.addEventListener('click', function(event) {
+    console.log('click');
+    if (event.target.tagName === 'IMG') {
+      handleImageClick(event);
     }
   });
-  
+}
+
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+  if (message.action === 'startImageListener') {
+    startImageListener();
+  }
+});
--- interface.js
+++ interface.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
+    console.log('Document has loaded');
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       chrome.tabs.sendMessage(tabs[0].id, {action: "startImageListener"});
     });
